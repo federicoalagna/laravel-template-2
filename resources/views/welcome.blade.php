c0af5dea2221126f974fbfde26219320d3774f5a
federicoalagna <161328407+federicoalagna@users.noreply.github.com>
2024-07-09 16:03:05 +0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Template</title>
+    @vite('resources/js/app.js')
+</head>
+
+<body>
+    <img src="{{ Vite::asset('resources/img/logo.png') }}" alt="">
+</body>
+
+</html>
